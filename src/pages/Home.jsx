9d2f4ba10c8274959626660500f07aae3b676764
EJ <119761899+ernestojustino9@users.noreply.github.com>
2025-07-08 05:22:45 +0100

    vai

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import Header from "../components/layouts/Header";
-import Welcomehero from "./welcomehero/Welcomehero";
-import About from "./about/About";
-import Skills from "./skills/Skills";
-import Service from "./service/Service";
+import Carocel from "./carocel/Carocel";
 import Contact from "./contact/Contact";
-import Projecto from "./projecto/Projecto";
 import BlogRecente from "./blog/BlogRecente";
 
-
 const Home = () => {
-
-  // 
+  //
   return (
     <div>
       <Header />
-      <Welcomehero />
-      <About/>
-      <Skills/>
-      <Service/>
-      <Projecto/>
-      <BlogRecente/>
-      <Contact/>
+      <Carocel />
+      <BlogRecente />
+      <Contact />
     </div>
   );
 };
